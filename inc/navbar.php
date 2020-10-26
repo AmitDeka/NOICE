c02521fd492cf4bb30ec79f5b369090d2567ddbf
AmitDeka <amitdeka49@gmail.com>
2020-10-26 21:28:24 +0530

    NOICE Update 26.10.9p

---
+++ inc/navbar.php
@@ -0,0 +1,34 @@
+<!-- navT -->
+
+<nav class="nav">
+        <div class="container-fluid">
+            <div class="logo">
+                <a href="#">noice</a>
+            </div>
+            <div id="mainListDiv" class="main_list">
+                <ul class="navlinks">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Design</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Testimonial</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+                <!-- <div class="fas-icon">
+                    <div class="icon-1">
+                        <i class="fas fa-search"></i>
+                    </div>
+                    <div class="icon-2">
+                        <i class="fas fa-shopping-cart"></i>
+                    </div>
+                </div> -->
+            </div>
+
+            
+
+            <span class="navTrigger">
+                <i></i>
+                <i></i>
+                <i></i>
+            </span>
+        </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+include "./inc/connc.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choyce</title>
+    <meta name="description" content="Custom T-Shirt Designer Website">
+
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/master.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/navbar.js" defer></script>
+</head>
+<body>
+    <header>
+        <?php include"./inc/navbar.php"?>
+    </header>
+    
+    <div class="content">
+        <img src="imgs/content.jpg" alt="">
+        <img src="imgs/content.jpg" alt="">
+    </div>
+</body>
+</html>
+
+
